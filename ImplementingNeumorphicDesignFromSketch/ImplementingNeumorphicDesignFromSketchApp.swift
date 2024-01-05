9a349f84b44e6c9c80838f7293825b756a55aa8e
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2024-01-05 11:16:13 +0000

    Initial Commit

---
+++ ImplementingNeumorphicDesignFromSketch/ImplementingNeumorphicDesignFromSketchApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImplementingNeumorphicDesignFromSketchApp.swift
+//  ImplementingNeumorphicDesignFromSketch
+//
+//  Created by Ryan Gallagher on 05/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct ImplementingNeumorphicDesignFromSketchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
