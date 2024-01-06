2df7b109fbaf19a78a7f9a8f95a091b134fc0329
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2024-01-06 16:30:44 +0000

    ButtonStyleGuide: added view for developing custom button styles

---
+++ ImplementingNeumorphicDesignFromSketch/StyleGuide/ButtonStyleGuide.swift
@@ -0,0 +1,21 @@
+//
+//  ButtonStyleGuide.swift
+//  ImplementingNeumorphicDesignFromSketch
+//
+//  Created by Ryan Gallagher on 06/01/2024.
+//
+
+import SwiftUI
+
+struct ButtonStyleGuide: View {
+    var body: some View {
+        VStack {
+            Text("Button Styles").font(.largeTitle)
+            
+        }
+    }
+}
+
+#Preview {
+    ButtonStyleGuide()
+}
