45397206f2e03c1ba98a04c4d7583eb8c14cd710
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2024-01-09 17:15:05 +0000

    added ShadowStyles view as will make reusable component for shadows being used across different button

---
+++ ImplementingNeumorphicDesignFromSketch/StyleGuide/ShadowStyles.swift
@@ -0,0 +1,20 @@
+//
+//  ShadowStyles.swift
+//  ImplementingNeumorphicDesignFromSketch
+//
+//  Created by Ryan Gallagher on 09/01/2024.
+//
+
+import SwiftUI
+
+struct ShadowStyles: View {
+    var body: some View {
+        VStack {
+            Text("Shadows")
+        }
+    }
+}
+
+#Preview {
+    ShadowStyles()
+}
