7362a446aea5bc3c28cc27000d25f138f92cce58
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2024-01-09 17:25:01 +0000

    ShadowStyles: added more colour blocks to test NeumorphicShadow struct ViewModifier

--- ImplementingNeumorphicDesignFromSketch/StyleGuide/ShadowStyles.swift
+++ ImplementingNeumorphicDesignFromSketch/StyleGuide/ShadowStyles.swift
@@ -10,11 +10,22 @@ import SwiftUI
 struct ShadowStyles: View {
     var body: some View {
         VStack {
+            Color("AccentColor")
+                .frame(width: 100, height: 100)
+                .cornerRadius(20)
+                .padding(30)
             Text("Shadows")
             
             Color("AccentColor")
                 .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 100)
                 .cornerRadius(20)
+                .padding(30)
+                .modifier(NeumorphicShadow(isLight: false))
+            
+            Color("AccentColor")
+                .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 100)
+                .cornerRadius(20)
+                .modifier(NeumorphicShadow(isLight: true))
         }
     }
 }
