9a8233b42cb923d11166f73ffd860f0f42aa4b32
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2024-01-09 17:16:57 +0000

    ShadowStyles: added colour block for comparison

--- ImplementingNeumorphicDesignFromSketch/StyleGuide/ShadowStyles.swift
+++ ImplementingNeumorphicDesignFromSketch/StyleGuide/ShadowStyles.swift
@@ -11,6 +11,10 @@ struct ShadowStyles: View {
     var body: some View {
         VStack {
             Text("Shadows")
+            
+            Color("AccentColor")
+                .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 100)
+                .cornerRadius(20)
         }
     }
 }
